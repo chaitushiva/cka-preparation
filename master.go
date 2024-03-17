230d2ca0919faf32649ae277eea5344e45ee20bb
chai <32284393+chaitushiva@users.noreply.github.com>
2024-03-17 00:23:25 -0400

    Update master.go

--- master.go
+++ master.go
@@ -11,27 +11,60 @@ func main() {
 		return
 	}
 
-	setupTerminal()
+	if err := setupTerminal(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	disableSwap()
+	if err := disableSwap(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	removePackages()
+	if err := removePackages(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	installPodman()
+	if err := installPodman(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	installPackages()
+	if err := installPackages(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	installContainerd()
+	if err := installContainerd(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	configureContainerd()
+	if err := configureContainerd(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	startServices()
+	if err := startServices(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	initializeK8s()
+	if err := initializeK8s(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	installCNI()
+	if err := installCNI(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	installEtcdctl()
+	if err := installEtcdctl(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 }
 
 func checkUbuntuVersion() error {
@@ -45,7 +78,7 @@ func checkUbuntuVersion() error {
 	return nil
 }
 
-func setupTerminal() {
+func setupTerminal() error {
 	cmd := `
 		apt-get --allow-unauthenticated update
 		apt-get --allow-unauthenticated install -y bash-completion binutils
@@ -59,24 +92,43 @@ func setupTerminal() {
 		echo 'complete -F __start_kubectl k' >> ~/.bashrc
 		sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
 	`
-	runShellCommand(cmd)
+	if err := runShellCommand(cmd); err != nil {
+		return err
+	}
+	return nil
 }
 
-func disableSwap() {
-	runShellCommand("swapoff -a")
-	runShellCommand("sed -i '/\\sswap\\s/ s/^\\(.*\\)$/#\\1/g' /etc/fstab")
+func disableSwap() error {
+	cmds := []string{
+		"swapoff -a",
+		"sed -i '/\\sswap\\s/ s/^\\(.*\\)$/#\\1/g' /etc/fstab",
+	}
+	for _, cmd := range cmds {
+		if err := runShellCommand(cmd); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func removePackages() {
-	runShellCommand("kubeadm reset -f || true")
-	runShellCommand("crictl rm --force $(crictl ps -a -q) || true")
-	runShellCommand("apt-mark unhold kubelet kubeadm kubectl kubernetes-cni || true")
-	runShellCommand("apt-get remove -y docker.io containerd kubelet kubeadm kubectl kubernetes-cni || true")
-	runShellCommand("apt-get autoremove -y")
-	runShellCommand("systemctl daemon-reload")
+func removePackages() error {
+	cmds := []string{
+		"kubeadm reset -f || true",
+		"crictl rm --force $(crictl ps -a -q) || true",
+		"apt-mark unhold kubelet kubeadm kubectl kubernetes-cni || true",
+		"apt-get remove -y docker.io containerd kubelet kubeadm kubectl kubernetes-cni || true",
+		"apt-get autoremove -y",
+		"systemctl daemon-reload",
+	}
+	for _, cmd := range cmds {
+		if err := runShellCommand(cmd); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func installPodman() {
+func installPodman() error {
 	cmd := `
 		. /etc/os-release
 		echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list
@@ -89,10 +141,13 @@ func installPodman() {
 		registries = ['docker.io']
 		EOF
 	`
-	runShellCommand(cmd)
+	if err := runShellCommand(cmd); err != nil {
+		return err
+	}
+	return nil
 }
 
-func installPackages() {
+func installPackages() error {
 	cmd := `
 		apt-get install -y apt-transport-https ca-certificates
 		mkdir -p /etc/apt/keyrings
@@ -110,20 +165,31 @@ func installPackages() {
 		apt-get --allow-unauthenticated install -y docker.io containerd kubelet=${KUBE_VERSION}-1.1 kubeadm=${KUBE_VERSION}-1.1 kubectl=${KUBE_VERSION}-1.1 kubernetes-cni
 		apt-mark hold kubelet kubeadm kubectl kubernetes-cni
 	`
-	runShellCommand(cmd)
+	if err := runShellCommand(cmd); err != nil {
+		return err
+	}
+	return nil
 }
 
-func installContainerd() {
-	runShellCommand("wget https://github.com/containerd/containerd/releases/download/v1.6.12/containerd-1.6.12-linux-amd64.tar.gz")
-	runShellCommand("tar xvf containerd-1.6.12-linux-amd64.tar.gz")
-	runShellCommand("systemctl stop containerd")
-	runShellCommand("mv bin/* /usr/bin")
-	runShellCommand("rm -rf bin containerd-1.6.12-linux-amd64.tar.gz")
-	runShellCommand("systemctl unmask containerd")
-	runShellCommand("systemctl start containerd")
+func installContainerd() error {
+	cmds := []string{
+		"wget https://github.com/containerd/containerd/releases/download/v1.6.12/containerd-1.6.12-linux-amd64.tar.gz",
+		"tar xvf containerd-1.6.12-linux-amd64.tar.gz",
+		"systemctl stop containerd",
+		"mv bin/* /usr/bin",
+		"rm -rf bin containerd-1.6.12-linux-amd64.tar.gz",
+		"systemctl unmask containerd",
+		"systemctl start containerd",
+	}
+	for _, cmd := range cmds {
+		if err := runShellCommand(cmd); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func configureContainerd() {
+func configureContainerd() error {
 	cmd := `
 		cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 		overlay
@@ -139,31 +205,49 @@ func configureContainerd() {
 		sudo sysctl --system
 		sudo mkdir -p /etc/containerd
 	`
-	runShellCommand(cmd)
+	if err := runShellCommand(cmd); err != nil {
+		return err
+	}
+	return nil
 }
 
-func startServices() {
+func startServices() error {
 	cmd := `
 		systemctl daemon-reload
 		systemctl enable containerd
 		systemctl restart containerd
 		systemctl enable kubelet && systemctl start kubelet
 	`
-	runShellCommand(cmd)
+	if err := runShellCommand(cmd); err != nil {
+		return err
+	}
+	return nil
 }
 
-func initializeK8s() {
-	runShellCommand("rm /root/.kube/config || true")
-	runShellCommand("kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print --pod-network-cidr 192.168.0.0/16")
-	runShellCommand("mkdir -p ~/.kube")
-	runShellCommand("sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config")
+func initializeK8s() error {
+	cmd := `
+		rm /root/.kube/config || true
+		kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print --pod-network-cidr 192.168.0.0/16
+		mkdir -p ~/.kube
+		sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
+	`
+	if err := runShellCommand(cmd); err != nil {
+		return err
+	}
+	return nil
 }
 
-func installCNI() {
-	runShellCommand("kubectl apply -f https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/calico.yaml")
+func installCNI() error {
+	cmd := `
+		kubectl apply -f https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/calico.yaml
+	`
+	if err := runShellCommand(cmd); err != nil {
+		return err
+	}
+	return nil
 }
 
-func installEtcdctl() {
+func installEtcdctl() error {
 	cmd := `
 		ETCDCTL_VERSION=v3.5.1
 		ETCDCTL_ARCH=$(dpkg --print-architecture)
@@ -173,12 +257,17 @@ func installEtcdctl() {
 		mv ${ETCDCTL_VERSION_FULL}/etcdctl /usr/bin/
 		rm -rf ${ETCDCTL_VERSION_FULL} ${ETCDCTL_VERSION_FULL}.tar.gz
 	`
-	runShellCommand(cmd)
+	if err := runShellCommand(cmd); err != nil {
+		return err
+	}
+	return nil
 }
 
-func runShellCommand(cmd string) {
+func runShellCommand(cmd string) error {
 	out, err := exec.Command("/bin/sh", "-c", cmd).CombinedOutput()
 	if err != nil {
-		fmt.Printf("Error running command: %s, output: %s\n", cmd, string(out))
+		return fmt.Errorf("Error running command: %s, output: %s", cmd, string(out))
 	}
+	return nil
 }
+
